f359cfdd747fc79247910de53d521cf4b6932766
Kevin Channon <kevinchannon@gmail.com>
2023-03-14 20:29:38 +0000

    CSV: Adding new values to a row

--- files/csv.hpp
+++ files/csv.hpp
@@ -1,21 +1,35 @@
 #pragma once
 
-#include <vector>
 #include <cstddef>
+#include <cstdint>
+#include <vector>
 
 namespace files {
 
-  class csv {
-  public:
-
-    class cell {};
-
-    [[nodiscard]] size_t rows() const { return _rows; }
-    [[nodiscard]] size_t cols() const { return _cols; }
-
-  private:
-    std::vector<cell> _cells;
-    size_t _rows{};
-    size_t _cols{};
+class csv {
+  struct cell {
+    uint64_t value;
   };
-} // namespace: files
+
+ public:
+
+  [[nodiscard]] size_t rows() const { return _rows; }
+  [[nodiscard]] size_t cols() const { return _cols; }
+
+  csv& operator<<(uint64_t val) {
+    _cells.emplace_back(val);
+    ++_cols;
+    if (0 == _rows) {
+      ++_rows;
+    }
+
+    return *this;
+  }
+
+ private:
+  std::vector<cell> _cells;
+  size_t _rows{};
+  size_t _cols{};
+};
+
+}  // namespace files
--- test/csv.test.cpp
+++ test/csv.test.cpp
@@ -12,4 +12,22 @@ TEST_CASE("CSV tests")
       SECTION("zero columns")  REQUIRE(0 == files::csv{}.cols());
     }
   }
+
+  SECTION("Adding values") {
+    SECTION("Adds a new column") {
+      SECTION("In a new row if there are no existing rows") {
+        auto csv = files::csv{};
+        csv << 1;
+
+        REQUIRE(1 == csv.rows());
+        REQUIRE(1 == csv.cols());
+
+        SECTION("And in an existing row if there is at least one") {
+          csv << 2;
+          REQUIRE(1 == csv.rows());
+          REQUIRE(2 == csv.cols());
+        }
+      }
+    }
+  }
 }
